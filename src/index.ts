1d83af03f6141cbfbb815858485ed459221f5721
almeigab <gabrielalmeida1403@hotmail.com>
2023-06-23 04:26:51 -0300

    feat: add router

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import { AppDataSource } from './data-source';
+import router from './router';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(express.json());
 app.use(morgan('tiny'));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server!');
-});
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
