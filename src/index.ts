a30162034a6ef6d6d9b34509e6956241d7de77ea
almeigab <gabrielalmeida1403@hotmail.com>
2023-06-22 23:38:56 -0300

    feat: add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(morgan('tiny'));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
