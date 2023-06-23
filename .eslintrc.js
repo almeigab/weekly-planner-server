f6f33752b1ddf8505b60d7e8f3512073b76b6a35
almeigab <gabrielalmeida1403@hotmail.com>
2023-06-22 23:22:37 -0300

    chore: setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    "prettier",  
+  ],
+  ignorePatterns: ['/*', '!/src'],
+  rules: {
+    "prettier/prettier": "error"
+  },
+};
