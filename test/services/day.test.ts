063b160ffc5d253856a4561626ae4cd060407335
almeigab <gabrielalmeida1403@hotmail.com>
2023-06-23 22:15:07 -0300

    fix: day service tests should be async

--- test/services/day.test.ts
+++ test/services/day.test.ts
@@ -14,13 +14,13 @@ describe('day service', () => {
     } as unknown as Repository<ObjectLiteral>);
   });
   describe('getDay', () => {
-    it('should return day', () => {
+    it('should return day', async () => {
       findOne.mockResolvedValue({
         id: 1,
         date: new Date('2023-06-23'),
       });
 
-      expect(
+      await expect(
         dayService.getDay(new GetDayDTO('2023-06-23'))
       ).resolves.toMatchObject({
         id: 1,
@@ -30,14 +30,14 @@ describe('day service', () => {
         where: { date: new Date('2023-06-23T00:00:00.000Z') },
       });
     });
-    it('should return day with activities', () => {
+    it('should return day with activities', async () => {
       findOne.mockResolvedValue({
         id: 1,
         date: new Date('2023-06-23'),
         activities: [],
       });
 
-      expect(
+      await expect(
         dayService.getDay(new GetDayDTO('2023-06-23'), true)
       ).resolves.toMatchObject({
         id: 1,
@@ -51,13 +51,13 @@ describe('day service', () => {
     });
   });
   describe('addDay', () => {
-    it('shoud return added day', () => {
+    it('shoud return added day', async () => {
       save.mockResolvedValue({
         id: 1,
         date: new Date('2023-06-23'),
       });
 
-      expect(
+      await expect(
         dayService.addDay(new AddDayDTO('2023-06-23'))
       ).resolves.toMatchObject({
         id: 1,
