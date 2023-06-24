f60bd3b60d81861cb224ae2db03b66af0493d2d5
almeigab <gabrielalmeida1403@hotmail.com>
2023-06-23 23:17:03 -0300

    test: remove Day entity

--- test/services/activity.test.ts
+++ test/services/activity.test.ts
@@ -1,12 +1,9 @@
 import { ObjectLiteral, Repository } from 'typeorm';
 import { AppDataSource } from '../../src/data-source';
-import dayService from '../../src/services/day';
 import activityService from '../../src/services/activity';
 import { AddActivityDTO } from '../../src/dto/activity';
 
 const save = jest.fn();
-dayService.getDay = jest.fn();
-dayService.addDay = jest.fn();
 
 describe('activity service', () => {
   beforeEach(() => {
@@ -16,98 +13,31 @@ describe('activity service', () => {
   });
   describe('addActivity', () => {
     it('shoud return added activity', async () => {
-      (dayService.getDay as jest.Mock).mockResolvedValue({
-        id: 1,
-        date: new Date('2023-06-23'),
-      });
-
       save.mockResolvedValue({
         id: 1,
         name: 'test',
+        weekDay: 0,
         from: new Date('0001-01-01T07:00:00Z'),
         to: new Date('0001-01-01T08:00:00Z'),
         checked: false,
-        day: {
-          id: 1,
-          date: new Date('2023-06-23'),
-        },
       });
 
       await expect(
         activityService.addActivity(
           new AddActivityDTO(
             'test',
+            0,
             '2023-06-23T07:00:00Z',
-            '2023-06-23T08:00:00Z',
-            '2023-06-23T00:00:00Z'
+            '2023-06-23T08:00:00Z'
           )
         )
       ).resolves.toMatchObject({
         id: 1,
         name: 'test',
+        weekDay: 0,
         from: new Date('0001-01-01T07:00:00Z'),
         to: new Date('0001-01-01T08:00:00Z'),
         checked: false,
-        day: {
-          id: 1,
-          date: new Date('2023-06-23'),
-        },
-      });
-      expect(dayService.getDay).toHaveBeenCalledWith(
-        {
-          date: new Date('2023-06-23'),
-        },
-        false
-      );
-      expect(dayService.addDay).not.toHaveBeenCalled();
-    });
-    it('shoud add day and return added activity', async () => {
-      (dayService.getDay as jest.Mock).mockResolvedValue(null);
-      (dayService.addDay as jest.Mock).mockResolvedValue({
-        id: 1,
-        date: new Date('2023-06-23'),
-      });
-
-      save.mockResolvedValue({
-        id: 1,
-        name: 'test',
-        from: new Date('0001-01-01T07:00:00Z'),
-        to: new Date('0001-01-01T08:00:00Z'),
-        checked: false,
-        day: {
-          id: 1,
-          date: new Date('2023-06-23'),
-        },
-      });
-
-      await expect(
-        activityService.addActivity(
-          new AddActivityDTO(
-            'test',
-            '2023-06-23T07:00:00Z',
-            '2023-06-23T08:00:00Z',
-            '2023-06-23T00:00:00Z'
-          )
-        )
-      ).resolves.toMatchObject({
-        id: 1,
-        name: 'test',
-        from: new Date('0001-01-01T07:00:00Z'),
-        to: new Date('0001-01-01T08:00:00Z'),
-        checked: false,
-        day: {
-          id: 1,
-          date: new Date('2023-06-23'),
-        },
-      });
-      expect(dayService.getDay).toHaveBeenCalledWith(
-        {
-          date: new Date('2023-06-23'),
-        },
-        false
-      );
-      expect(dayService.addDay).toHaveBeenCalledWith({
-        date: new Date('2023-06-23'),
       });
     });
   });
