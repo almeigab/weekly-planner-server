27950f91897271e3a60e5408989b43244b7de7d1
almeigab <gabrielalmeida1403@hotmail.com>
2023-06-24 01:27:09 -0300

    test: add test case when activity overlaps

--- test/services/activity.test.ts
+++ test/services/activity.test.ts
@@ -1,18 +1,24 @@
 import { ObjectLiteral, Repository } from 'typeorm';
+import { AssertionError } from 'assert';
 import { AppDataSource } from '../../src/data-source';
 import activityService from '../../src/services/activity';
 import { AddActivityDTO } from '../../src/dto/activity';
+import { ActivityOverlapsError } from '../../src/utils/errors/ActivityOverlapsError';
 
 const save = jest.fn();
+const countBy = jest.fn();
 
 describe('activity service', () => {
   beforeEach(() => {
     jest.spyOn(AppDataSource, 'getRepository').mockReturnValue({
       save,
+      countBy,
     } as unknown as Repository<ObjectLiteral>);
   });
   describe('addActivity', () => {
     it('shoud return added activity', async () => {
+      countBy.mockResolvedValue(0);
+
       save.mockResolvedValue({
         id: 1,
         name: 'test',
@@ -39,6 +45,20 @@ describe('activity service', () => {
         to: new Date('0001-01-01T08:00:00Z'),
         checked: false,
       });
+    });
+    it('shoud throw error when activity overlaps', async () => {
+      countBy.mockResolvedValue(1);
+
+      await expect(
+        activityService.addActivity(
+          new AddActivityDTO(
+            'test',
+            0,
+            '2023-06-23T07:00:00Z',
+            '2023-06-23T08:00:00Z'
+          )
+        )
+      ).rejects.toThrowError(new ActivityOverlapsError());
     });
   });
 });
